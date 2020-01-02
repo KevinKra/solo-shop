cefbf068d1564ab6ce61152630723df0072139a8
Kevin Krato <kratok08@gmail.com>
2020-01-01 17:07:34 -0700

    Update routes to support registration related pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/register", to: "users#new"
+  post "/register", to: "users#create"
+
+  get "/profile", to: "users#index"
 end
