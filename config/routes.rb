7921543d7ac84484216968ac0525796bef58d420
Kevin Krato <kratok08@gmail.com>
2020-01-02 12:06:08 -0700

    Create session login and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get "/register", to: "users#new"
   post "/register", to: "users#create"
 
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+
   get "/profile", to: "users#index"
 end
