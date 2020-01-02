36ad7bef9fa0d443c1cd572c8af2eb61a55dbdfe
Kevin Krato <kratok08@gmail.com>
2020-01-02 11:05:13 -0700

    Create factory for user generation

---
+++ spec/factories/user.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+  factory :user do
+  name  { Faker::Name.first_name }
+  street_address { Faker::Address.street_address }
+  city { Faker::Address.city }
+  state { Faker::Address.state }
+  zip_code { Faker::Address.zip }
+  email { Faker::Internet.email }
+  password { "password" }
+  password_confirmation { "password" }
+    trait :default_user do
+      role { 0 }
+    end
+    trait :merchant_user do
+      role { 1 }
+    end
+    trait :admin_user do
+      role { 2 }
+    end
+  end
+end
