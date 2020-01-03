4216cb3997ae8869b30a7bdc262fbd4142ebe650
Kevin Krato <kratok08@gmail.com>
2020-01-02 17:19:20 -0700

    Build out ApplicationController to handle current user behaviors and provide helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+  helper_method :current_user, :user_role
+
+  def current_user 
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] != nil
+  end
+
+  def user_role(type)
+    @current_user.role == type if @current_user
+  end
 end
